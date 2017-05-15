6d82ae9f4475013f6bd472229a1c0cb5da55659c
MashaSamoylova <mashanedyak@gmail.com>
2017-05-15 15:36:20 +0700

    for Andrew112134

--- big.h
+++ big.h
@@ -35,6 +35,7 @@ class Big {
 		Big Mul(base);
 		Big Div(base, base&);
 		friend int Compare(const Big &b, const Big &a);
+		friend Big Substraction(Big &b, Big &a);
 
 
 		Big& operator = (const Big &a); // this = a
